e2a525f1913dadd3ca5e193efa8d72fce7827742
k1ssmate123 <kissmate001@gmail.com>
2023-08-09 11:44:28 +0200

    inputrequest

--- i4pTest/Program.cs
+++ i4pTest/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace i4pTest
 {
+
+    class Encoding
+    {
+        string message;
+        string key;
+       public string Message { get { return message; } }
+        public string Key { get { return key; } }
+
+        public Encoding(string inputMessage, string encryptionKey)
+        {
+            message = inputMessage;
+            key = encryptionKey;
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Message: ");
+            string message = Console.ReadLine();
+            Console.Write("Key: ");
+            string key = Console.ReadLine();
+            Encoding encode = new Encoding(message, key);
         }
     }
 }
