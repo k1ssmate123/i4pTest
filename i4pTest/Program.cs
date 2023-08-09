e1f958cd476a281eccf308972d0b3602658501dd
k1ssmate123 <kissmate001@gmail.com>
2023-08-09 12:41:26 +0200

    decryption is done, 1st version

--- i4pTest/Program.cs
+++ i4pTest/Program.cs
@@ -1,49 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Runtime.Remoting.Messaging;
 
 namespace i4pTest
 {
-
-    class Encoding
+    
+    public class Encoding
     {
-        string message;
-        string key;
+        protected string message;
+       protected string key;
         public string Message { get { return message; } }
         public string Key { get { return key; } }
 
-        List<char> characters = new List<char>();
+        protected List<char> characters = GetCharacters();
 
         public Encoding(string inputMessage, string encryptionKey)
         {
             message = inputMessage;
             key = encryptionKey;
         }
-       
-        void GetCharacters()
+        public Encoding()
         {
+            
+        }
+
+        static List<char> GetCharacters()
+        {
+            List<char> result = new List<char>();
             for (int i = 97; i < 123; i++)
             {
-                characters.Add((char)i);  //Filling up the charlist with ascii letters - 97 - a, 122 - z
+                result.Add((char)i);  //Filling up the charlist with ascii letters - 97 - a, 122 - z
             }
-            characters.Add(' ');
+            result.Add(' ');
+            return result;
         }
 
         public string Encrypting()
         {
-            GetCharacters();
-            string encryptedMsg="";
+
+            string encryptedMsg = "";
 
             for (int i = 0; i < message.Length; i++)
             {
-                int sums = ReturnCharCode(message[i])+ReturnCharCode(key[i]);
+                int sums = ReturnCharCode(message[i]) + ReturnCharCode(key[i]);
                 if (sums > 26)
                 {
                     sums %= 27;
                 }
-              
+
                 encryptedMsg += characters[sums];
             }
 
@@ -51,35 +56,87 @@ namespace i4pTest
 
         }
 
-        int ReturnCharCode(char letter)
+        protected int ReturnCharCode(char letter)
         {
             int j = 0;
-            while(j < characters.Count&& characters[j] != letter)
+            while (j < characters.Count && characters[j] != letter)
             {
                 j++;
             }
-            if(j < characters.Count)
+            if (j < characters.Count)
             {
                 return j;
             }
             return -1;
 
         }
-       
+
 
     }
+
+    class Decoding : Encoding
+    {
+        public Decoding(string inputMessage, string decryptionKey)
+        {
+            message = inputMessage;
+            key = decryptionKey;
+        }
+
+        public string Decrypting()
+        {
+            string decryptedMsg = "";
+      
+            for (int i = 0; i < message.Length; i++)
+            {
+                int sum = ReturnCharCode(message[i]) - ReturnCharCode(key[i]);
+                if(sum < 0)
+                {
+                  
+                    sum = 27 + sum;
+                }
+                decryptedMsg += characters[sum];
+            }
+            return decryptedMsg;
+
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            TaskTwo();
+        }
+
+        static void TaskOne()
+        {
+            string message, key;
             Console.Write("Message: ");
-            string message = Console.ReadLine();
-            Console.Write("Key: ");
-            string key = Console.ReadLine();
+            message = Console.ReadLine();
+            do
+            {
+                Console.Write("Key: ");
+                key = Console.ReadLine();
+            }
+            while (key.Length < message.Length);
             Encoding encode = new Encoding(message, key);
-            Console.WriteLine(encode.Encrypting());
+            Console.WriteLine("The encrypted message is: " + encode.Encrypting());
+        }
 
-
+        static void TaskTwo()
+        {
+            string encMessage, key;
+            Console.Write("Encrypted message: ");
+            encMessage = Console.ReadLine();
+            do
+            {
+                Console.Write("Key: ");
+                key = Console.ReadLine();
+            }
+            while (key.Length < encMessage.Length);
+            Decoding decoding = new Decoding(encMessage, key);
+            Console.WriteLine("The message is: "+decoding.Decrypting());
+          
         }
     }
 }
